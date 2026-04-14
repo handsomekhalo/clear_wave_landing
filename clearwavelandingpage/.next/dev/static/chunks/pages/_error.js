e1e07913e8eef723b292c3afff603b20165c3508
Titus <handsomekhalo@gmail.com>
2026-04-14 10:09:30 +0200

    removed folder  structure and redid the stryture

---
+++ clearwavelandingpage/.next/dev/static/chunks/pages/_error.js
@@ -0,0 +1,13 @@
+__turbopack_load_page_chunks__("/_error", [
+  "static/chunks/node_modules_next_dist_compiled_0o6l_m6._.js",
+  "static/chunks/node_modules_next_dist_shared_lib_0~pg0mt._.js",
+  "static/chunks/node_modules_next_dist_client_0pe1dg-._.js",
+  "static/chunks/node_modules_next_dist_0k8zr~w._.js",
+  "static/chunks/0vx3_next_dist_pages__error_0fwhh18.js",
+  "static/chunks/[next]_entry_page-loader_ts_0uuo21p._.js",
+  "static/chunks/node_modules_react-dom_0bruynb._.js",
+  "static/chunks/node_modules_0lx093h._.js",
+  "static/chunks/[root-of-the-server]__09d7qt9._.js",
+  "static/chunks/[project]_clearwavelandingpage_pages__error_07xvfw~._.js",
+  "static/chunks/turbopack-[project]_clearwavelandingpage_pages__error_12g_qlj._.js"
+])
